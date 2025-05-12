de9a3e246e67b548374a37333f8990e69bc263c8
Qiyao Shi <qiyao.shi@neura-robotics.com>
2025-05-12 10:42:19 +0200

    Ping mason to v1.0 since new released version break some stuff

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,17 +1,36 @@
 return {
-  "williamboman/mason.nvim",
-  opts_extend = { "ensure_installed" },
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shfmt",
-      "ruff",
-      "pyright",
-      "marksman",
-      "yaml-language-server",
-      "prettier",
-      "clangd",
-      "lua-language-server",
+  -- Mason
+  {
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "ruff",
+        "pyright",
+        "marksman",
+        "yaml-language-server",
+        "prettier",
+        "clangd",
+        "lua-language-server",
+      },
     },
+  },
+
+  -- Mason LSPConfig
+  {
+    "mason-org/mason-lspconfig.nvim",
+    version = "^1.0.0",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "pyright",
+          "lua_ls",
+          "clangd",
+        },
+        automatic_installation = true,
+      })
+    end,
   },
 }
